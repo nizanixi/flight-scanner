e25478768dac4e0186e4c5c575e2b3ada172f5ef
ivan_nizic <ivan.nizic@rimac-technology.com>
2024-12-02 21:32:44 +0100

    Create empty web API project that will be used on server side of application

---
+++ source/FlightScanner.WebApi/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        CreateWebBuilder(builder);
+
+        var app = builder.Build();
+
+        ConfigureMiddleware(app);
+
+        app.Run();
+    }
+
+    private static void CreateWebBuilder(WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+    }
+
+    private static void ConfigureMiddleware(WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+    }
+}
